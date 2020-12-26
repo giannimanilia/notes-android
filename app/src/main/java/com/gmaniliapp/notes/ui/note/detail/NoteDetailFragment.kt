afc2464448623d516a1aec962fed75f6d14afe8e
Gianni Antonio Manilia <giannimanilia@gmail.com>
2020-12-26 12:21:38 +0100

    implemented note details

--- app/src/main/java/com/gmaniliapp/notes/ui/note/detail/NoteDetailFragment.kt
+++ app/src/main/java/com/gmaniliapp/notes/ui/note/detail/NoteDetailFragment.kt
@@ -2,23 +2,63 @@ package com.gmaniliapp.notes.ui.note.detail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.gmaniliapp.notes.R
+import com.gmaniliapp.notes.data.local.entities.Note
 import com.gmaniliapp.notes.ui.BaseFragment
+import com.gmaniliapp.notes.util.Status
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_modify_note.*
 import kotlinx.android.synthetic.main.fragment_note_detail.*
 
+@AndroidEntryPoint
 class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
+
+    private val viewModel: NoteDetailViewModel by viewModels()
 
     private val args: NoteDetailFragmentArgs by navArgs()
 
+    private var currentNote: Note? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (args.id.isNotEmpty()) {
+            viewModel.selectNoteById(args.id)
+        }
+
+        subscribeToObservers()
 
         fabEditNote.setOnClickListener {
             findNavController().navigate(
                 NoteDetailFragmentDirections.actionNoteDetailFragmentToModifyNoteFragment(args.id)
             )
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.note.observe(viewLifecycleOwner, Observer {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        result.data?.let {
+                            val note = result.data
+                            currentNote = note
+                            tvNoteTitle.text = note.title
+                            tvNoteContent.text = note.content
+                        }
+                    }
+                    Status.ERROR -> {
+                        showSnackBar("Unexpected error")
+                    }
+                    Status.LOADING -> {
+
+                    }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/gmaniliapp/notes/ui/note/detail/NoteDetailViewModel.kt
+++ app/src/main/java/com/gmaniliapp/notes/ui/note/detail/NoteDetailViewModel.kt
@@ -1,6 +1,30 @@
 package com.gmaniliapp.notes.ui.note.detail
 
+import android.content.SharedPreferences
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gmaniliapp.notes.data.local.entities.Note
+import com.gmaniliapp.notes.data.repository.NoteRepository
+import com.gmaniliapp.notes.util.Event
+import com.gmaniliapp.notes.util.Resource
+import kotlinx.coroutines.launch
 
-class NoteDetailViewModel : ViewModel() {
+class NoteDetailViewModel @ViewModelInject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    private val _note = MutableLiveData<Event<Resource<Note>>>()
+    val note: LiveData<Event<Resource<Note>>>
+        get() = _note
+
+    fun selectNoteById(noteId: String) = viewModelScope.launch {
+        _note.postValue(Event(Resource.loading(null)))
+        val note = repository.selectNoteById(noteId)
+        note?.let {
+            _note.postValue(Event(Resource.success(it)))
+        } ?: _note.postValue(Event(Resource.error(null)))
+    }
 }
